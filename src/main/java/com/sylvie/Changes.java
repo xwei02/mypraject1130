78bb4649615e07de0e01bbd83af2ed474b4d5963
Sylvie <sylvie.wu@hotmail.com>
2021-11-30 14:42:47 +0800

    Commit

--- src/main/java/com/sylvie/Changes.java
+++ src/main/java/com/sylvie/Changes.java
@@ -3,7 +3,9 @@ package com.sylvie;
 public class Changes {
     public static void main(String[] args) {
         int n = 28;
-        System.out.println(n%5);
-        System.out.println(n/10);
+        System.out.println("10x"+(n/10));
+        System.out.println("5x"+(n-(n/10)*10-n%5)/5);
+        System.out.println("1x"+(n%5));
+
     }
 }
--- src/main/java/com/sylvie/Vending.java
+++ src/main/java/com/sylvie/Vending.java
@@ -4,20 +4,43 @@ import java.util.Scanner;
 
 public class Vending {
     public static void main(String[] args) {
-        Scanner scanner = new  Scanner(System.in);
-        int total = 0;
-        boolean end = false;
-        while (!end){
-            System.out.println("Plaese put your coin 1/5/10 Total:" + total);
-            String s = scanner.next();
-            switch (s){
-                case "1":
-                case "5":
-                case "10":
-                    int n = Integer.parseInt(s);
-                    total += n;
+        int i = 0;
+        int[] prices = {25, 15, 10};
+        System.out.println(prices.length);
+        for (i = 0; i < prices.length; i++) {
+            System.out.println(prices[i]);
+        }
+            Scanner scanner = new Scanner(System.in);
+            int total = 0;
+            boolean end = false;
+            while (!end) {
+                System.out.println("Plaese put your coin 1/5/10 Total:" + total);
+                String s = scanner.next();
+                switch (s) {
+                    case "a":
+                    case "b":
+                    case "c":
+                        System.out.println(s.charAt(0)-97);
+                        int index = s.charAt(0)-97;
+                        if (total >= prices[index]) {
+                            System.out.println("DON!");
+                            total -= prices[index];
+                            break;
+                        } else {
+                            System.out.println("BEEP!");
+                        }
+                        int n = Integer.parseInt(s);
+                        //total=total+n
+                        total += n;
+                        //i=i+1
+                        i++;
 
+                    default:
+                        System.out.println("Answer Wrong!");
+                        break;
+
+
+                }
             }
         }
     }
-}
